6e00d9b719e35083c6f28c7bb2ffd8dc3c2648c5
Fufek <62938195+Fufek-git@users.noreply.github.com>
2024-04-21 10:50:37 +0000

    Removed hotswapping extensions

--- apps/chat.py
+++ apps/chat.py
@@ -1,4 +1,4 @@
-from interactions import listen, Extension, Task
+from interactions import listen, Extension, max_concurrency
 
 from hugchat import hugchat
 from hugchat.login import Login
@@ -24,28 +24,22 @@ class Chat(Extension):
         if event.message.author == self.bot.user:
             return
         if event.message.channel.name == "chatting":
-            generate.start()
-        return event
-        
-@Task.create()
-async def generate(event):
-    messages1 = await fetch_message_history(event.message.channel)
-    msg = await event.message.channel.send("*Generating...*")
-    retries = 5
-    for _ in range(retries):
-        try:
-            response_tokens = []
-            for resp in chatbot.query(messages1 + "Klyda:", stream=True):
-                if resp is not None and "token" in resp:
-                    response_tokens.append(resp["token"])
-                    response = ("".join(response_tokens))
-                    print("Response:", response)
-                    await msg.edit(content=(response))
-            if not response_tokens:
-                print("Response tokens empty, retrying...")
-                continue
+            messages1 = await fetch_message_history(event.message.channel)
+            msg = await event.message.channel.send("*Generating...*")
+            retries = 5
+            for _ in range(retries):
+                try:
+                    response_tokens = []
+                    for resp in chatbot.query(messages1 + "Klyda:", stream=True):
+                        if resp is not None and "token" in resp:
+                            response_tokens.append(resp["token"])
+                            response = ("".join(response_tokens))
+                            print("Response:", response)
+                            await msg.edit(content=(response))
+                    if not response_tokens:
+                        print("Response tokens empty, retrying...")
+                        continue
 
-            break
-        except Exception as e:
-            print(f"An exception occurred: {e}, retrying...")
-
+                    break
+                except Exception as e:
+                    print(f"An exception occurred: {e}, retrying...")
