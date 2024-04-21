c496871ba65cd6e27e11e8386b3df547e13e2a9d
Fufek <62938195+Fufek-git@users.noreply.github.com>
2024-04-21 09:32:55 +0000

    Removed "omaga" extension

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ bot.load_extension("apps.nie")
 bot.load_extension("apps.quote")
 bot.load_extension("commands.purge")
 bot.load_extension("commands.ask")
-bot.load_extension("apps.omaga")
 bot.load_extension("apps.chat")
 
 bot.start('MTIyNzY3NjA0ODgxMzA2ODM3OQ.GVtb_R.JrTDAa-wVeibuQo9pTyX_YgzHcTMjuM08DXTyU')
