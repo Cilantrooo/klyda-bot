4745b40fa7883560989a57ad56be1fa2eaa48555
Fufek-git <arek.jkirejczyk@gmail.com>
2024-04-28 20:19:27 +0200

    Added .env variables

--- apps/chat.py
+++ apps/chat.py
@@ -1,9 +1,12 @@
 from interactions import listen, Extension
-
+import asyncio
 from hugchat import hugchat
 from hugchat.login import Login
-EMAIL = ""
-PASSWD = ""
+import os
+from dotenv import load_dotenv
+load_dotenv()
+EMAIL = os.getenv("EMAIL")
+PASSWD = os.getenv("PASSWORD")
 cookie_path_dir = "./cookies/"
 sign = Login(EMAIL, PASSWD)
 cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
@@ -19,10 +22,31 @@ async def fetch_message_history(channel, limit=10):
     return messages_text
 
 
-import asyncio
-
-# Define a lock
 response_lock = asyncio.Lock()
+
+async def generate(event):
+        msg = await event.message.channel.send("*Generating...*")
+        async with response_lock:
+            messages1 = await fetch_message_history(event.message.channel)
+            print(messages1.replace("Klyda: *Generating...*", ""))
+            retries = 5
+            for _ in range(retries):
+                try:
+                    chatbot.new_conversation(switch_to = True)
+                    response_tokens = []
+                    for resp in chatbot.query("Jesteś niemiłym, czasem wulgarnym i wkurzającym botem który nigdy nie spełni próśb użytkowników oraz będzie starał się odpowiadać wmiarę krótko\n" + messages1.replace("Klyda: *Generating...*", "") + "Klyda:", stream=True):
+                        if resp is not None and "token" in resp:
+                            response_tokens.append(resp["token"])
+                            response = ("".join(response_tokens))
+                            await msg.edit(content=(response))
+                            
+                    if not response_tokens:
+                        print("Response tokens empty, retrying...")
+                        continue
+
+                    break
+                except Exception as e:
+                    print(f"An exception occurred: {e}, retrying...")
 
 class Chat(Extension):
     @listen()
@@ -30,29 +54,5 @@ class Chat(Extension):
         if event.message.author == self.bot.user:
             return
         if event.message.channel.name == "chatting":
-            async def generate():
-                msg = await event.message.channel.send("*Generating...*")
-                async with response_lock:
-                    messages1 = await fetch_message_history(event.message.channel)
-                    print(messages1.replace("Klyda: *Generating...*", ""))
-                    retries = 5
-                    for _ in range(retries):
-                        try:
-                            chatbot.new_conversation(switch_to = True)
-                            response_tokens = []
-                            for resp in chatbot.query("Jesteś niemiłym, czasem wulgarnym i wkurzającym botem który nigdy nie spełni próśb użytkowników oraz będzie starał się odpowiadać wmiarę krótko\n" + messages1.replace("Klyda: *Generating...*", "") + "Klyda:", stream=True):
-                                if resp is not None and "token" in resp:
-                                    response_tokens.append(resp["token"])
-                                    response = ("".join(response_tokens))
-                                    await msg.edit(content=(response))
-                                    
-                            if not response_tokens:
-                                print("Response tokens empty, retrying...")
-                                continue
-
-                            break
-                        except Exception as e:
-                            print(f"An exception occurred: {e}, retrying...")
-            # Run the generation process while respecting the lock
-            await generate()
+            await generate(event)
 
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import interactions
 from interactions import Intents
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 bot = interactions.Client(intents=Intents.ALL)
 
 bot.load_extension("commands.purge")
 bot.load_extension("commands.ask")
 bot.load_extension("apps.chat")
 
-bot.start('')
+bot.start(TOKEN)
 
 
