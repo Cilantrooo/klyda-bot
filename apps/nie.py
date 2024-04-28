f9a10b1fdef6f886bf0f727590c9d36c6878a41e
Fufek <62938195+Fufek-git@users.noreply.github.com>
2024-04-28 16:14:04 +0200

    Delete apps/nie.py

--- apps/nie.py
+++
@@ -1,6 +0,0 @@
-from interactions import message_context_menu, Message, Extension, ContextMenuContext
-class Nie(Extension):
-    @message_context_menu(name="nie")
-    async def repeat(self, ctx: ContextMenuContext):
-        message: Message = ctx.target
-        await ctx.send("NIE")
