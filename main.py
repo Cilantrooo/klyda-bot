c739629ba198e164cd72c2d2497f2e09ce25999a
Fufek <62938195+Fufek-git@users.noreply.github.com>
2024-04-28 16:25:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from interactions import Intents
 
 bot = interactions.Client(intents=Intents.ALL, token="" )
 
-bot.load_extension("apps.nie")
-bot.load_extension("apps.quote")
 bot.load_extension("commands.purge")
 bot.load_extension("commands.ask")
 bot.load_extension("apps.chat")
