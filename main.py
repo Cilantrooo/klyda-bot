78f08868fa4c259c9cc30f294fad73cd01e435f5
Fufek-git <arek.jkirejczyk@gmail.com>
2024-04-28 20:58:52 +0200

    Removed seperate env TOKEN

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from interactions import Intents
 import os
 from dotenv import load_dotenv
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
 bot = interactions.Client(intents=Intents.ALL)
 
 @interactions.listen()
@@ -14,6 +13,6 @@ bot.load_extension("commands.purge")
 bot.load_extension("commands.ask")
 bot.load_extension("apps.chat")
 
-bot.start(TOKEN)
+bot.start(os.getenv('TOKEN'))
 
 
